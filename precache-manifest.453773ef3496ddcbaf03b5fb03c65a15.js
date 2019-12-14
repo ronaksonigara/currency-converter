3a579b8a3e1f388b6595f7d1b71f9f9f9b23ac8c
Ronak Sonigara <ronak.sonigara.2018@gmail.com>
2019-12-14 23:02:02 +0530

    Updates

---
+++ precache-manifest.453773ef3496ddcbaf03b5fb03c65a15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e71946a83d6e7abafcae8df1b08053c2",
+    "url": "/currency-converter/index.html"
+  },
+  {
+    "revision": "ea538d5e72a2f87dad17",
+    "url": "/currency-converter/static/css/main.db1cb21f.chunk.css"
+  },
+  {
+    "revision": "4ce45d358145a166ceb6",
+    "url": "/currency-converter/static/js/2.add483c8.chunk.js"
+  },
+  {
+    "revision": "67935b6d863b3e23bc08cf31dbc40ecc",
+    "url": "/currency-converter/static/js/2.add483c8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ea538d5e72a2f87dad17",
+    "url": "/currency-converter/static/js/main.60add48f.chunk.js"
+  },
+  {
+    "revision": "5a02c9bfaa3d7b83668a",
+    "url": "/currency-converter/static/js/runtime-main.847a6513.js"
+  }
+]);
